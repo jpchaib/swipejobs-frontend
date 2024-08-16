a03355fa71a9a461b12929550e28853c129d9fa5
jpchaib <joaopedrochaib@gmail.com>
2024-08-16 18:30:18 +1000

    removed delay

--- src/services/matches.js
+++ src/services/matches.js
@@ -2,14 +2,12 @@ const apiUrl = process.env.REACT_APP_SWIPEJOBS_API_BASE_URL;
 
 export const getMatches = async (workerId) => {
     try {
-        delay(5000);
         const response = await fetch(`${apiUrl}worker/${workerId}/matches`);
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        delay(5000);
         const json = await response.json();
 
         if (!json || typeof json !== "object") {
